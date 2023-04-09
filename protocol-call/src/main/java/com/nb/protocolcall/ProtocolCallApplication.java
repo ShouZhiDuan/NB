3f8fd718ebf865681578eda2ed374fab64ec1c1c
shouzhi <shouzhi.duan2702@nvxclouds>
2023-04-09 10:46:09 +0800

    init project

---
+++ protocol-call/src/main/java/com/nb/protocolcall/ProtocolCallApplication.java
@@ -0,0 +1,13 @@
+package com.nb.protocolcall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProtocolCallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProtocolCallApplication.class, args);
+	}
+
+}
---
+++ protocol-call/src/test/java/com/nb/protocolcall/ProtocolCallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nb.protocolcall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProtocolCallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
