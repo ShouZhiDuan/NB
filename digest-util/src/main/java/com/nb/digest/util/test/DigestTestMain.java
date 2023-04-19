068d933ba494f9799f021bdc8c6b4e8b38429cfa
shouzhi <shouzhi.duan2702@nvxclouds>
2023-04-19 14:05:36 +0800

    添加digest测试功能

--- digest-util/src/main/java/com/nb/digest/util/test/DigestTestMain.java
+++ digest-util/src/main/java/com/nb/digest/util/test/DigestTestMain.java
@@ -9,22 +9,22 @@ public class DigestTestMain {
 
     private final  static  String NV_APP_KEY = "QWER@123456";
     private final  static  String NV_APP_SECRET = "66666656789012345678901234888888";
-    private final  static String salt = "2400c2f4412f4545ab8c523bfc0957af";
-    private final  static String nonce = "1681211671988";
+    private final  static String salt = "f4e9a6ffda9a4e7c8cf3260e65c71c16";
+    private final  static String nonce = "1681813725008";
 
     public static void main(String[] args){
         String uph = getUph(NV_APP_KEY, NV_APP_SECRET);
         String sph = getSph(salt, uph);
         String digest = DigestUtil.sha256Hex(sph + nonce);
-        System.err.println(digest);
+        System.err.println("digest = " + digest);
 
-        byte[] base64Token = Base64.getDecoder().decode("eyJleHBpcmVUaW1lIjoxNjgxMjk4MTQyMDQ2LCJzdGFydFRpbWUiOjE2ODEyMTE3NDIwNDZ9");
-        String tokenStr = new String(base64Token);
-        System.err.println(tokenStr);
+        //byte[] base64Token = Base64.getDecoder().decode("eyJleHBpcmVUaW1lIjoxNjgxMjk4MTQyMDQ2LCJzdGFydFRpbWUiOjE2ODEyMTE3NDIwNDZ9");
+        //String tokenStr = new String(base64Token);
+        //System.err.println(tokenStr);
 
 
-        Date date = new Date(1681298142046L);
-        System.out.println(date);
+        //Date date = new Date(1681298142046L);
+        //System.out.println(date);
 
 
 
