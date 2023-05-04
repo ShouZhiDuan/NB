7646c78a8a331a860641ac02e9b80377aca33357
duanshouzhi <shouzhi.duan2702@nvxclouds.com>
2023-05-04 19:14:16 +0800

    修复pagehelper依赖版本

--- demo/src/main/java/com/nb/demo/DemoApplication.java
+++ demo/src/main/java/com/nb/demo/DemoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 /**
  * @author dev
  */
-@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+@SpringBootApplication
 public class DemoApplication {
 
     public static void main(String[] args) {
