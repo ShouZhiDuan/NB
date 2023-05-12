ab3380e22ce21534d5997be7b9ade1d00c64b100
duanshouzhi <shouzhi.duan2702@nvxclouds.com>
2023-05-12 14:02:17 +0800

    fix code

--- starter-algo4j/src/main/java/com/nb/algo4j/StarterAlgo4jApplication.java
+++ starter-algo4j/src/main/java/com/nb/algo4j/StarterAlgo4jApplication.java
@@ -3,6 +3,9 @@ package com.nb.algo4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author dev
+ */
 @SpringBootApplication
 public class StarterAlgo4jApplication {
 
