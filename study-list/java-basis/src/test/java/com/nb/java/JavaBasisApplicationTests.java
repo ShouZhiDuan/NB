c0b1c675a48ea70b6021d9cf99f079b668777f43
duanshouzhi <shouzhi.duan2702@nvxclouds.com>
2023-05-25 14:44:46 +0800

    添加java学习模块

---
+++ study-list/java-basis/src/test/java/com/nb/java/JavaBasisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nb.java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaBasisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
