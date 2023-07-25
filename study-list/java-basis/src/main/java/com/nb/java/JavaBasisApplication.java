38a47e5c39ebe66d80a2e9468ae6321c5dcafc0d
duanshouzhi <shouzhi.duan2702@nvxclouds.com>
2023-07-25 16:33:19 +0800

    fix code

--- study-list/java-basis/src/main/java/com/nb/java/JavaBasisApplication.java
+++ study-list/java-basis/src/main/java/com/nb/java/JavaBasisApplication.java
@@ -5,15 +5,17 @@ import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
-
-import java.util.ArrayList;
-import java.util.List;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
 
 @SpringBootApplication
 public class JavaBasisApplication {
 
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException, InterruptedException {
 		SpringApplication springApplication = new SpringApplication(JavaBasisApplication.class);
 		ConfigurableApplicationContext run = springApplication.run(args);
 
@@ -22,15 +24,24 @@ public class JavaBasisApplication {
 		TestService testService = beanFactory.getBean(TestService.class);
 		testService.test();
 
-		List<byte[]> list = new ArrayList<>();
+//		List<byte[]> list = new ArrayList<>();
+//		new Thread(() -> {
+//			while (true){
+//				System.out.println("===每次添加一个byte[10]字节数组===");
+//				list.add(new byte[10]);
+//			}
+//		},"模拟Java运行时CPU、内存占用过高的线程").start();
+
+		/**
+		 * 模拟打开文件：内存泄漏
+		 */
+		while (true){
+			System.out.println("读取一次文件");
+			Thread.sleep(3000);
+			Stream<Path> list = Files.list(Paths.get("/root/test_conf"));
+		}
 
 
-		new Thread(() -> {
-			while (true){
-				System.out.println("===每次添加一个byte[10]字节数组===");
-				list.add(new byte[10]);
-			}
-		},"模拟Java运行时CPU、内存占用过高的线程").start();
 
 	}
 
